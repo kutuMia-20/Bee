bca2d3d3def18fe8e17280edb105f34821932ab8
kutuMia-20 <152077612+kutuMia-20@users.noreply.github.com>
2023-12-28 17:18:28 +0600

    3065.cpp

--- 3065.cpp
+++ 3065.cpp
@@ -1,1 +1,39 @@
+#include<iostream>
+#include<cstdlib>
+using namespace std;
 
+int main ()
+{
+    int x,y;
+    char c;
+    int sum = 0,i=0;
+
+    while(1)
+    {
+
+        i++;
+
+        cin >> x;
+
+        if(x==0)
+        {
+            break;
+        }
+        sum =0;
+
+        for(int i=0; i<x; i++)
+        {
+            cin >> y;
+
+            if(y < 0)
+            {
+                sum = sum+y;
+            }
+            else
+            {
+                sum = sum + y;
+            }
+        }
+        cout <<  "Teste "<< i << endl<< sum << endl << endl;
+    }
+}
